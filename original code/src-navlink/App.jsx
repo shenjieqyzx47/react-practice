61b4d4c76a8ed5888bcbf95bc534821a624f8829
shenjieqyzx47 <41566120+shenjieqyzx47@users.noreply.github.com>
2021-08-23 23:24:15 +1000

    react-practice

--- original code/src-navlink/App.jsx
+++ original code/src-navlink/App.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import {Route} from 'react-router-dom'
+import {NavLink,Route} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
-import MyNavLink from './components/MyNavLink'
 
 
 export default class App extends Component {
@@ -20,10 +19,8 @@ export default class App extends Component {
                         <div className="list-group">
                         {/* <a className="list-group-item active" href="./about.html">About</a>
                         <a className="list-group-item" href="./home.html">Home</a> */}
-                                {/* <NavLink activeClassName="active" className="list-group-item" to="/about">About</NavLink>
-                                <NavLink activeClassName="active" className="list-group-item" to="/home">Home</NavLink> */}
-                            <MyNavLink to="about">About</MyNavLink>
-                            <MyNavLink to="home">Home</MyNavLink>
+                                <NavLink activeClassName="active" className="list-group-item" to="/about">About</NavLink>
+                                <NavLink activeClassName="active" className="list-group-item" to="/home">Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
